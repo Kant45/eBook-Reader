ee421bbb9a4ad4c0ebae83933549a2bcbc2689c9
Kantemir Tsavkilov <kantemir0073@gmail.com>
2023-03-05 22:18:55 +0300

    Replaced WebBrowser to FlowDocument

--- eBook Reader/ViewModel/ReadBookViewModel.cs
+++ eBook Reader/ViewModel/ReadBookViewModel.cs
@@ -3,10 +3,13 @@ using eBook_Reader.Model;
 using HtmlAgilityPack;
 using System.Collections.Generic;
 using VersOne.Epub;
+using VersOne.Epub.Schema;
 using System.IO;
 using System.Windows.Input;
 using eBook_Reader.Commands;
 using eBook_Reader.Stores;
+using System.Text;
+using System.Windows.Documents;
 
 namespace eBook_Reader.ViewModel;
 
@@ -16,6 +19,7 @@ public class ReadBookViewModel : ViewModelBase {
     private readonly EpubContent m_epubContent;
     private String m_selectedHtml;
     private List<EpubTextContentFile> m_readingOrder;
+    private FlowDocument m_flowDocument;
 
     public Book SelectedBook { 
         get => m_selectedBook;
@@ -29,6 +33,13 @@ public class ReadBookViewModel : ViewModelBase {
         set {
             m_selectedHtml = value;
             OnPropertyChanged("SelectedHtml");
+        }
+    }
+    public FlowDocument FlowDocument {
+        get => m_flowDocument;
+        set {
+            m_flowDocument = value;
+            OnPropertyChanged("FlowDocument");
         }
     }
     public List<EpubTextContentFile> ReadingOrder {
@@ -45,7 +56,13 @@ public class ReadBookViewModel : ViewModelBase {
         m_epubContent = m_selectedBook.EBook.Content;
         m_readingOrder = m_selectedBook.EBook.ReadingOrder;
 
-        m_selectedHtml = m_readingOrder[1].Content;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(Int32 i = 1; i < m_readingOrder.Count - 1; i++) {
+            stringBuilder.Append(GetContentFileText(m_readingOrder[i]));
+        }
+
+        m_selectedHtml = stringBuilder.ToString();
 
         PreviousPageCommand = new PreviousPageCommand(this);
         NextPageCommand = new NextPageCommand(this);
@@ -53,4 +70,16 @@ public class ReadBookViewModel : ViewModelBase {
 
     public ICommand PreviousPageCommand { get; protected set; }
     public ICommand NextPageCommand { get; protected set; }
+
+    private static StringBuilder GetContentFileText(EpubTextContentFile textContentFile) {
+        HtmlDocument htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(textContentFile.Content);
+        StringBuilder sb = new StringBuilder();
+
+        foreach(HtmlNode node in htmlDocument.DocumentNode.SelectNodes("//text()")) {
+            sb.AppendLine(node.InnerText.Trim());
+        }
+
+        return sb;
+    }
 }
