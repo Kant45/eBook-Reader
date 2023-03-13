ba230e49708d26bbee349c43d37ebf897df1d235
Kantemir Tsavkilov <kantemir0073@gmail.com>
2023-03-13 23:12:48 +0300

    Added animation

---
+++ eBook Reader/Commands/SortCommand.cs
@@ -0,0 +1,70 @@
+﻿using eBook_Reader.Model;
+using eBook_Reader.ViewModel;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBook_Reader.Commands
+{
+    class SortCommand : CommandBase {
+
+        private AllBooksViewModel m_viewModel;
+
+        public SortCommand(AllBooksViewModel viewModel) {
+            m_viewModel = viewModel;
+        }
+        public override void Execute(Object parameter) {
+
+            String SelectedSortProperty = m_viewModel.SelectedSortParameter.Name;
+            ObservableCollection<Book> books = m_viewModel.BookList;
+
+            List<Book> tempList;
+
+            switch(SelectedSortProperty) {
+
+                case "TitleUp": {
+
+                    tempList = books.OrderBy(book => book.Title).ToList();
+
+                    for(Int32 i = 0; i < tempList.Count; i++) {
+                        books.Move(books.IndexOf(tempList[i]), i);
+                    }
+                    break;
+                }
+                case "TitleDown": {
+
+                    tempList = books.OrderByDescending(book => book.Title).ToList();
+
+                    for(Int32 i = 0; i < tempList.Count; i++) {
+                        books.Move(books.IndexOf(tempList[i]), i);
+                    }
+                    break;
+                }
+                case "AuthorUp": {
+
+                    tempList = books.OrderBy(book => book.Author).ToList();
+
+                    for(Int32 i = 0; i < tempList.Count; i++) {
+                        books.Move(books.IndexOf(tempList[i]), i);
+                    }
+                    break;
+                }
+                case "AuthorDown": {
+
+                    tempList = books.OrderByDescending(book => book.Author).ToList();
+
+                    for(Int32 i = 0; i < tempList.Count; i++) {
+                        books.Move(books.IndexOf(tempList[i]), i);
+                    }
+                    break;
+                }
+                default: return;
+            }
+
+        }
+    }
+}
--- eBook Reader/ViewModel/AllBooksViewModel.cs
+++ eBook Reader/ViewModel/AllBooksViewModel.cs
@@ -7,27 +7,23 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using static System.Reflection.Metadata.BlobBuilder;
+using System.Windows.Shell;
 
 namespace eBook_Reader.ViewModel {
     public class AllBooksViewModel : ViewModelBase {
 
         private ObservableCollection<Book> m_bookList;
-        private static Book m_selectedBook;
+        private static Book? m_selectedBook;
         private readonly NavigationStore m_navigationStore;
-        private WindowState m_currentWindowState;
-        public WindowState CurrentWindowState {
-            get {
-                return m_currentWindowState;
-            }
-            set {
-                m_currentWindowState = value;
-                base.OnPropertyChanged("CurWindowState");
-            }
-        }
+        private SortParameter m_selectedSortParameter;
+        private ObservableCollection<SortParameter> m_sortParameters;
+
         public Book SelectedBook {
             get { return m_selectedBook; }
             set {
@@ -35,12 +31,27 @@ namespace eBook_Reader.ViewModel {
                 OnPropertyChanged("SlectedBook");
             }
         }
-        public ICommand AddEpubBookCommand { get; protected set; }
-        public NavigateReadBookCommand NavigateReadBookCommand { get; set; }
-
         public ObservableCollection<Book> BookList {
             get { return m_bookList; }
         }
+        public SortParameter SelectedSortParameter {
+            get => m_selectedSortParameter;
+            set {
+                m_selectedSortParameter = value;
+                OnPropertyChanged("SelectedSortParameter");
+            }
+        }
+        public ObservableCollection<SortParameter> SortParameters {
+            get => m_sortParameters;
+            set {
+                m_sortParameters = value;
+                OnPropertyChanged("SortParameters");
+            }
+        }
+
+        public ICommand AddEpubBookCommand { get; protected set; }
+        public ICommand NavigateReadBookCommand { get; protected set; }
+        public ICommand SortCommand { get; protected set; }
 
         public AllBooksViewModel(NavigationStore navigationStore) {
 
@@ -48,20 +59,42 @@ namespace eBook_Reader.ViewModel {
 
             m_navigationStore = navigationStore;
 
-            NavigateReadBookCommand = new NavigateReadBookCommand(m_navigationStore);
-            
-
             String[] filePaths = Directory.GetFiles("Library");
+
+            List<Book> sortableList = new List<Book>();
 
             foreach(String fPath in filePaths) {
                 Book book = new Book(fPath);
 
-                m_bookList.Add(book);
+                sortableList.Add(book);
             }
 
-            CurrentWindowState = WindowState.Normal;
+            sortableList = sortableList.OrderBy(book => book.Title).ToList();
 
+            for(Int32 i = 0; i < sortableList.Count; i++) {
+                BookList.Add(sortableList[i]);
+            }
+
+            SortParameters = new ObservableCollection<SortParameter>() {
+                new SortParameter("TitleUp","/Icons/Sort/sort_up_icon.png"),
+                new SortParameter("TitleDown","/Icons/Sort/sort_down_icon.png"),
+                new SortParameter("AuthorUp","/Icons/Sort/sort_up_icon.png"),
+                new SortParameter("AuthorDown","/Icons/Sort/sort_down_icon.png")
+            };
+            SelectedSortParameter = SortParameters[0];
+
+            NavigateReadBookCommand = new NavigateReadBookCommand(m_navigationStore);
             AddEpubBookCommand = new AddBookCommand(m_navigationStore, this);
+            SortCommand = new SortCommand(this);
+        }
+    }
+    public class SortParameter {
+        public String Name { get; set; }
+        public String ImagePath { get; set; }
+
+        public SortParameter(String name, String imagePath) {
+            Name = name;
+            ImagePath = imagePath;
         }
     }
 }
